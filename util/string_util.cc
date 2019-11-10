88bc6a2a6409b0223442e517c631777ed417ae54
Blobfishman <luis.kreienbrink@gmail.com>
2019-11-10 23:26:24 +0100

    String util added
    added string contains function

---
+++ util/string_util.cc
@@ -0,0 +1,18 @@
+#include "util/string_util.h"
+
+/**
+ * @brief string_contains counts the number of c inside the string
+ * @param str target string
+ * @param length string length
+ * @param c search char
+ * @return number of c inside the string
+ */
+int string_contains(char *str, unsigned length, char c) {
+  int count = 0;
+  for (unsigned i = 0; i < length; i++) {
+    if (str[i] == c) {
+      count++;
+    }
+  }
+  return count;
+}
---
+++ util/string_util.h
@@ -0,0 +1,6 @@
+#ifndef __string_util_include
+#define __string_util_include
+
+int string_contains(char *str, unsigned length, char c);
+
+#endif // __string_util_include
